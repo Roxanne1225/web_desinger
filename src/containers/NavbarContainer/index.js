2fadf0082522106d71155f5759cc19471a7a68d2
Roxanne1225 <roxannnne1225@gmail.com>
2020-07-17 12:25:46 -0500

    initial structure setup commit not done yet

---
+++ src/containers/NavbarContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarHeader from '../../components/NavbarHeader'
+
+function NavbarContainer() {
+    return (
+        <div>
+           <NavbarHeader />
+
+        </div>
+    );
+}
+
+
+export default NavbarContainer;
